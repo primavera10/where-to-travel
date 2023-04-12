9accd9d4a744d6729eefae21269a4ea6330d44e4
Liza <l.yalak@flatlogic.com>
2023-04-12 21:39:16 +0200

    refactor: autocomplete component

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,20 @@
         'mb-14': autocompletedCities.length === 0,
       }">
       <div class="mt-10  flex items-center gap-8">
-        <div class="relative">
-          <input
-            type="text"
-            v-model="city"
-            class="focus:outline-none text-xl"
-            @keydown.enter="checkData"
-            placeholder="Enter your city" />
-          <Autocomplete class="w-full py-4 pr-4 top-full bg-white absolute z-50 mb-8 mt-2 autocompleteShadow rounded-lg pl-2"
-                        v-if="autocompletedCities.length!== 0"
-                        @select="fillCity"
-                        :autocompleted-cities="autocompletedCities" />
-        </div>
+        <Autocomplete
+        v-model="city"/>
+<!--        <div class="relative">-->
+<!--          <input-->
+<!--            type="text"-->
+<!--            v-model="city"-->
+<!--            class="focus:outline-none text-xl"-->
+<!--            @keydown.enter="checkData"-->
+<!--            placeholder="Enter your city" />-->
+<!--          <Autocomplete class="w-full py-4 pr-4 top-full bg-white absolute z-50 mb-8 mt-2 autocompleteShadow rounded-lg pl-2"-->
+<!--                        v-if="autocompletedCities.length!== 0"-->
+<!--                        @select="fillCity"-->
+<!--                        :autocompleted-cities="autocompletedCities" />-->
+<!--        </div>-->
         <button class="bg-skyBlue rounded-lg p-3 text-white cursor-pointer hover:bg-darkBlue" @click="checkData">
           Search
         </button>
@@ -65,7 +67,6 @@
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
-import { useDebounceFn } from "@vueuse/core";
 import Autocomplete from "@/components/Autocomplete.vue";
 
 
@@ -102,55 +103,11 @@ watch(response, () => {
     writeRadius();
   }
 });
-watch(city, () => {
-  if (city.value !== "") {
-    debounce();
-  }
-  if (city.value === "") {
-    autocompletedCities.value = [];
-  }
-});
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "e014dd2be7mshb4c842b876ec2c4p1eb946jsncaa5e0348633",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
-  }
-};
-
-
-let getAutoCompletedCities = async function() {
-  return await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city.value}&limit=10`, options)
-    .then((e) => e.json());
-};
-
-const writeAutocompletedCities = async () => {
-  if (city.value === "") {
-    return autocompletedCities.value = [];
-  }
-  let data = await getAutoCompletedCities();
-  let a = data.data.map((obj: object) => Object.values(obj));
-  autocompletedCities.value = a.map((elem: Array<any>) => {
-    return elem[3];
-  });
-};
-
-const debounce = useDebounceFn(writeAutocompletedCities, 700);
-
-function fillCity(name: string) {
-  city.value = name;
-  checkData();
-}
 
 function hasRadius() {
   return (radius.value !== "") ? radius.value : "2000";
 }
 
 </script>
-<style scoped>
-.autocompleteShadow {
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-}
-</style>
+
 
